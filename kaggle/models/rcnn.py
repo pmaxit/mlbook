253082e1824f2dbba9a41ac9d3ce72aeabfffc7c
puneet <puneetgirdhar.in@gmail.com>
2022-07-23 19:10:21 -0500

    improving object detection

--- kaggle/models/rcnn.py
+++ kaggle/models/rcnn.py
@@ -9,23 +9,28 @@ from torchvision.ops import nms, box_convert
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torch
+from typing import Iterable 
 
 class LitWheat(pl.LightningModule):
     def __init__(self, n_classes, learning_rate=1e-4 ):
         super().__init__()
+        self.save_hyperparameters()
         self.detector = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         in_features = self.detector.roi_heads.box_predictor.cls_score.in_features
-        self.detector.roi_heads.box_predictor = FastRCNNPredictor(in_features, n_classes)
-        self.lr = learning_rate
+        self.detector.roi_heads.box_predictor = FastRCNNPredictor(in_features, self.hparams.n_classes)
         
     def forward(self, imgs, targets=None):
         # TorchVision FasterRCNN return the loss during the training
+        
+        opt = self.optimizers()
+        self.detector.train()
+        loss_dict = self.detector(imgs, targets)
+        loss = sum(loss for loss in loss_dict.values())
+        opt.zero_grad()
+        
         self.detector.eval()
-        return self.detector(imgs)
-    
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-    
+        outputs = self.detector(imgs)
+        return {'loss': loss, 'output': outputs}
     
     def training_step(self, batch, batch_idx):
         pixel_values = batch['pixel_values']
@@ -34,36 +39,45 @@ class LitWheat(pl.LightningModule):
         loss_dict = self.detector(pixel_values, labels)
         loss = sum(loss for loss in loss_dict.values())
         self.log_dict(loss_dict)
-        return {'loss': loss, 'log':loss_dict}
+        return loss
     
     def validation_step(self, batch, batch_idx):
         pixel_values = batch['pixel_values']
         boxes = batch['labels']
-        pred_boxes = self.forward(pixel_values)
+        outputs = self.forward(pixel_values, boxes)
         
-        accuracy = torch.mean(torch.stack([self.accuracy(b['boxes'], pb['boxes'], iou_threshold=0.5 ) for b, pb in zip(boxes, pred_boxes)]))
-        metrics = {'val_accuracy': accuracy}
+        accuracy = torch.mean(torch.stack([self.accuracy(b['boxes'], pb['boxes'], iou_threshold=0.75 ) for b, pb in zip(boxes, outputs['output'])]))
+        metrics = {'val_accuracy': accuracy, 'val_loss': outputs['loss']}
         self.log_dict(metrics)
         
-        return metrics
+        return outputs['loss']
         
     def test_step(self, batch, batch_idx):
         pixel_values = batch['pixel_values']
         boxes = batch['labels']
-        pred_boxes = self.forward(pixel_values)
+        outputs = self.forward(pixel_values, boxes)
         
-        test_accuracy = torch.mean(torch.stack([self.accuracy(b['boxes'], pb['boxes'], iou_threshold=0.5 ) for b, pb in zip(boxes, pred_boxes)]))
-        metrics = {'test_accuracy': test_accuracy}
-        return self.test_accuracy
+        test_accuracy = torch.mean(torch.stack([self.accuracy(b['boxes'], pb['boxes'], iou_threshold=0.75 ) for b, pb in zip(boxes, outputs['output'])]))
+        metrics = {'test_accuracy': test_accuracy, 'test_loss': outputs['loss']}
+        
+        return metrics
+        
         
     
     def configure_optimizers(self):
         params = [p for p in self.detector.parameters() if p.requires_grad]
-        optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+        optimizer = torch.optim.SGD(params, lr=self.hparams.learning_rate, momentum=0.9, weight_decay=0.0005)
 
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
         return [optimizer], [lr_scheduler]
+    
+    def predict_step(self, batch, batch_idx):
+        pixel_values = batch['pixel_values']
+
+        with torch.no_grad():
+            output = self.detector(pixel_values)
+        return output
     
     def accuracy(self, src_boxes, pred_boxes, iou_threshold=0.5):
         """ The accuracy method is not the one used in the evaluator but very similar"""
--- kaggle/object_detection.py
+++ kaggle/object_detection.py
@@ -3,11 +3,15 @@
 # For example, here's several helpful packages to load
 
 import cv2
+import sys
 import re
 import math
 import os, ast
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib
+matplotlib.use('TkAgg')
 
 from matplotlib import pyplot as plt # plotting
 import matplotlib.patches as patches
@@ -55,6 +59,8 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 
 from functools import partial
+from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
+from typing import Iterable
 
 dataset_path = Path('/extras/Data')
 dataset_img_train = dataset_path/'train'
@@ -141,6 +147,15 @@ def annotate_examples(examples, idx):
         
     return examples
 
+def image_test_transform(example, transform=None):
+    image = np.array(example['images'][0])
+    if transform is not None:
+        transformed = transform(image=image)
+        image = transformed['image']
+    example['pixel_values'] = [image]
+    
+    return example
+ 
 def image_transform(example, transform=None):
     image = np.array(example['images'][0])
     boxes = np.array(example['boxes'][0])
@@ -157,8 +172,43 @@ def image_transform(example, transform=None):
     example['pixel_values'] = [image]
     example['boxes'] = [boxes]
     return example
-        
+
+def predict_box(model_path:str, file_paths:Iterable[str],draw=True,threshold=0.6):
+    test_ds = (Dataset.from_dict({'images':file_paths})
+           .cast_column('images',Image())
+           .with_transform(partial(image_test_transform, transform=get_test_transforms()))
+          )
+    trainer = pl.Trainer(accelerator="gpu", devices=[0])
+
+    test_dl = DataLoader(test_ds,batch_size=1, shuffle=True, collate_fn=partial(collate_fn,test=True), num_workers=1)
+    model = LitWheat.load_from_checkpoint(model_path)
+    predictions = trainer.predict(model, test_dl)
+    print("length of predictions ", len(predictions))
     
+    if draw:
+        images = []
+        
+        # remember we need to add '0' to the predictions array
+        # because our batch_size is 1
+        
+        # if our batch size were 2 then then each predictions will be 
+        # 2 dimensional array
+        for image, boxes in zip(test_ds, predictions):
+            img_rgb = image['images'].resize((256, 256))
+            img_rgb_np = np.array(img_rgb)
+            boxes = boxes[0]
+               
+            for box, score in zip(boxes['boxes'].cpu().numpy(), boxes['scores'].cpu().numpy()):  
+                if score > threshold:
+                    cv2.rectangle(img_rgb_np,
+                        (box[0], box[1]),
+                        (box[2], box[3]),
+                        (220, 0, 0), 2)
+            
+            images.append(img_rgb_np)
+
+        return images, predictions
+    return predictions
 
 def prepare_data():
     train_df = pd.read_csv(dataset_path/'train.csv')
@@ -183,7 +233,6 @@ def prepare_data():
     ds = Dataset.from_pandas(combined_train)
     processed_ds = (ds.map(annotate_examples,batched=False, with_indices=True)
                 .cast_column('images', Image(decode=True))
-               
                )
     
     dds = processed_ds.train_test_split(test_size=0.2)
@@ -203,15 +252,16 @@ def collate_fn(batch,test=False):
     for example in batch:
         # apply train transforms
         image = example['pixel_values']
-        label = {
-            'boxes' : example['boxes'].float(),
-            'labels': torch.tensor(example['labels'],dtype=torch.int64),
-            'area': torch.tensor(example['area']),
-            'iscrowd': torch.tensor(example['iscrowd']),
-            'image_id': torch.tensor(example['image_id'])
-        }
-        labels.append(label)
         images.append(image)
+        if not test:
+            label = {
+                'boxes' : example['boxes'].float(),
+                'labels': torch.tensor(example['labels'],dtype=torch.int64),
+                'area': torch.tensor(example['area']),
+                'iscrowd': torch.tensor(example['iscrowd']),
+                'image_id': torch.tensor(example['image_id'])
+            }
+            labels.append(label)
         
     pixel_values = torch.stack(images)
     if not test:
@@ -224,10 +274,9 @@ def get_dataloaders(dds):
         dds['train'],
         batch_size=8, shuffle=True, collate_fn=collate_fn,
         num_workers=4),
-        DataLoader(dds['test'],batch_size=8, shuffle=True, collate_fn=collate_fn, num_workers=4))
+        DataLoader(dds['test'],batch_size=8, shuffle=False, collate_fn=collate_fn, num_workers=4))
 
-
-def main():
+def train():
     dds = prepare_data()
     
     early_stop_callback = EarlyStopping(
@@ -239,10 +288,20 @@ def main():
     )
     
     logger = WandbLogger(project=WANDB_PROJECT, log_model=True)
-    trainer = pl.Trainer( gpus=2, progress_bar_refresh_rate=1, 
-                         max_epochs=4,deterministic=False, precision=16,
-                         logger = logger,callbacks=[early_stop_callback],
-                         distributed_backend='ddp_spawn')
+    checkpoint_callback =  ModelCheckpoint(monitor='val_accuracy',mode='max',
+                                             filename='{epoch}-{step}--{val_loss:.1f}',
+                                             save_top_k=1,dirpath="./models/wheat-detection/")
+    
+    trainer = pl.Trainer(max_epochs=4, 
+                         logger = logger,
+                         callbacks=[
+                            early_stop_callback, 
+                            TQDMProgressBar(refresh_rate=2),
+                            checkpoint_callback
+                        ],
+                        
+                        accelerator='gpu',
+                        fast_dev_run=False)
 
     detector = LitWheat(2)
     train_dl, test_dl = get_dataloaders(dds)
@@ -251,6 +310,28 @@ def main():
     # save the model
     torch.save(detector,"models/model.pth")
     
+def predict():
+    from PIL import Image
+    import glob
+    model_path = sys.argv[1]
+
+    images = glob.glob('/extras/Data/test/*.jpg')
+    print(images)
+    
+    imgs, predictinos = predict_box(model_path, images)
+    for id, img in enumerate(imgs):
+        im = Image.fromarray(img)
+        output_filename = f'output/output_{id}.jpg'
+        im.save(output_filename)
+    # fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+    # ax.set_axis_off()
+    
+    # ax.imshow(imgs[0])
+    # plt.show()
+    # plt.pause(3)
     
 if __name__ == '__main__':
-    main()
+    train()
+    #predict()
+ 
+    
